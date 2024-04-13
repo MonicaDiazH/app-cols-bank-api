afb4cfea6ea9db71cd8abff7791bf741c7a944ac
Monica Diaz <monica.diaz@ingeneo.com.co>
2024-04-13 15:41:00 -0600

    Initial files.

---
+++ src/main/java/com/cols/bank/transactions/AppColsBankApiApplication.java
@@ -0,0 +1,13 @@
+package com.cols.bank.transactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppColsBankApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppColsBankApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cols/bank/transactions/AppColsBankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cols.bank.transactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppColsBankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
